01ee54d56d351269d6aa498f9bca5250d1105df1
Yonson_ <taincior@gmail.com>
2019-01-20 17:59:06 +0100

    documentation sample

--- src/main/java/com/jestgit/egot/trasa/TrasaDTO.java
+++ src/main/java/com/jestgit/egot/trasa/TrasaDTO.java
@@ -8,6 +8,11 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+/**
+ * Class used to transfer data about Trasa between View layer and Controller layer.
+ * @author Michał Jończyk
+ * @version 1.2
+ */
 @Data
 public class TrasaDTO {
     private Long numerTrasy;
@@ -31,8 +36,21 @@ public class TrasaDTO {
 
     private Object id;
 
+    /**
+     * Default constructor used to pass TrasaDTO object to the View
+     */
     public TrasaDTO(){}
 
+    /**
+     * Parameterized constructor used to create TrasaDTO and initialize all of its fields
+     * @param numerTrasy - ID of Trasa
+     * @param grupa - Grupa related to this Trasa
+     * @param punktPoczatkowy - starting Punkt of Trasa
+     * @param punktKoncowy - end Punkt of Trasa
+     * @param punktyZaTrase - number of points you get for completing Trasa
+     * @param opis - description of  rasa
+     * @param id - Id of Przewodnik who added this Trasa to database
+     */
     public TrasaDTO(Long numerTrasy, String grupa, Long punktPoczatkowy, Long punktKoncowy, String punktyZaTrase, String opis, Object id){
         this.numerTrasy = numerTrasy;
         this.nazwaGrupy = grupa;
