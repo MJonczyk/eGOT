ece92711c12f73826733751c29b7eb971e82f93f
mateusz-wojcik <mateuszwojcik170@gmail.com>
2019-01-22 22:34:10 +0100

    simple ajax functionality added

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,3 +9,31 @@ window.addEventListener('load', function (ev) {
 function toMenu() {
     window.location.replace('http://localhost:8080/index');
 };
+
+var aktualnosci = ["Cos", "Cos innego", "Cos jeszcze"];
+var licznik = 0;
+
+// $(document).ready(function () {
+//     $("#dalejA").click(function (e) {
+//         e.preventDefault();
+//         $('#indexOpisDiv').innerText = aktualnosci[licznik % aktualnosci.length];
+//         licznik++;
+//     });
+// });
+
+
+var myRequest = new XMLHttpRequest();
+// myRequest.open('GET', '../aktualnosci/ak1.html');
+
+myRequest.onreadystatechange = function (ev) {
+    if(myRequest.readyState === 4) {
+        document.getElementById('indexOpisDiv').innerHTML = myRequest.responseText;
+    }
+};
+
+function sendAJAX() {
+    licznik++;
+    myRequest.open('GET', '../aktualnosci/ak' + (licznik) + '.html');
+    licznik = (licznik % 3);
+    myRequest.send();
+}
